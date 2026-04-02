6e602927e8b21eaf8ea19a45ef56abbe173e1fe3
Faraz ahmed <119921031+Farazkhan111@users.noreply.github.com>
2026-04-02 15:31:50 +0530

    Update Imgpage1.jsx

--- Frontend/src/Frontpage/Imgpage1.jsx
+++ Frontend/src/Frontpage/Imgpage1.jsx
@@ -10,7 +10,7 @@ export default function Imgpage1() {
           <div className="row ">
             <div className="col-sm-3"></div>
             <div className="sec1s1 col-sm-6">
-               <h6 className='fs-5'>Welcome To <span className='text-info'>MyGaMeS</span> </h6>
+               <h6 className='fs-5'>Welcome To <span className='text-info'>MyRePacks</span> </h6>
            <h1 className='fs-2'><span className='text-info'>Browse</span> Our Popular Games Here</h1>
            <Link to={"/collection"} className='btn btn-primary btn-lg mt-3'>Browse Now</Link>
             </div>
